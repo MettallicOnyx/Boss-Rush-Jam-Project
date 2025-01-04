656d4ddf6c979124a1bd2113448d2015f0664bd3
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-01-04 16:31:19 +0100

    added gradual knockback

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -5,7 +5,7 @@ const BULLET = preload("res://scenes/bullet.tscn")
 @onready var muzzle: Marker2D = $Marker2D
 @onready var player: CharacterBody2D = $".."
 
-var recoil_strength: float = 20.0
+var recoil_strength: float = 300.0
 
 func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
@@ -19,6 +19,7 @@ func _process(_delta: float) -> void:
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("shoot"):
+		player.velocity = Vector2.ZERO
 		var bullet_instance = BULLET.instantiate()
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position = muzzle.global_position
@@ -27,4 +28,4 @@ func _input(_event: InputEvent) -> void:
 		
 		var recoil_angle := player.position.angle_to_point(get_global_mouse_position())
 		var recoil_dir := -Vector2.RIGHT.rotated(recoil_angle);
-		player.position += recoil_dir * recoil_strength;
+		player.velocity = recoil_dir * recoil_strength;
