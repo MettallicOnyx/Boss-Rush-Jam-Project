27c64344207943ccfcf57fe431224d868954525c
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-01-04 17:32:37 +0100

    bug fix

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -19,7 +19,6 @@ func _process(_delta: float) -> void:
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("shoot"):
-		player.velocity = Vector2.ZERO
 		var bullet_instance = BULLET.instantiate()
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position = muzzle.global_position
