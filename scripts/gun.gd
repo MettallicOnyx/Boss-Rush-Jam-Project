8ea14297f90886272807c0de90ceb620587918c5
Ollie <olibobadventuresyt@gmail.com>
2025-01-04 15:07:13 +0000

    recoil for gun

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -3,9 +3,11 @@ extends Node2D
 const BULLET = preload("res://scenes/bullet.tscn")
 
 @onready var muzzle: Marker2D = $Marker2D
+@onready var player: CharacterBody2D = $".."
 
+var recoil_strength: float = 20.0
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
 
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
@@ -13,8 +15,16 @@ func _process(delta: float) -> void:
 		scale.y = -1
 	else:
 		scale.y = 1
+
+
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		var bullet_instance = BULLET.instantiate()
 		get_tree().root.add_child(bullet_instance)
 		bullet_instance.global_position = muzzle.global_position
 		bullet_instance.rotation = rotation
+
+		
+		var recoil_angle := player.position.angle_to_point(get_global_mouse_position())
+		var recoil_dir := -Vector2.RIGHT.rotated(recoil_angle);
+		player.position += recoil_dir * recoil_strength;
